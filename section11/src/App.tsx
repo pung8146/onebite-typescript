27d307b7f3bf9bec40216f29164f9d6e454a22dc
pung8146 <pung8146@naver.com>
2023-06-28 13:24:14 +0900

    투두리스트 삭제 기능 구현

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor.tsx";
-
-interface Todo {
-  id: number;
-  content: string;
-}
+import { Todo } from "./types";
+import TodoItem from "./components/TodoItem.tsx";
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -22,6 +19,10 @@ function App() {
     ]);
   };
 
+  const onClickDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   useEffect(() => {
     console.log(todos);
   }, [todos]);
@@ -29,6 +30,12 @@ function App() {
     <div className="App">
       <h1>Todo</h1>
       <Editor onClickAdd={onClickAdd} />
+      <div>
+        {todos.map((todo) => (
+          // <div key={todo.id}>{todo.content}</div>
+          <TodoItem key={todo.id} {...todo} onClickDelete={onClickDelete} />
+        ))}
+      </div>
     </div>
   );
 }
