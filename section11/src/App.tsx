7f503959e33bb9a77ef13b804a856f491872a41e
pung8146 <pung8146@naver.com>
2023-06-28 11:23:08 +0900

    todoList(TypeScript) 리팩토링 useState 와 props 의 사용법1

---
+++ section11/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import Editor from "./components/Editor.tsx";
+
+interface Todo {
+  id: number;
+  content: string;
+}
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    setTodos([
+      ...todos,
+      {
+        id: idRef.current++,
+        content: text,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+      <Editor onClickAdd={onClickAdd} />
+    </div>
+  );
+}
+
+export default App;
